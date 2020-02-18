07fa9ca66114a415db5b8e48e450f36c5f3b9d77
Ryan Drobel <ryandrobel@gmail.com>
2020-02-18 14:58:26 -0600

    arrays push

---
+++ javascriptstck/arrays/predict2.js
@@ -0,0 +1,11 @@
+var arr = [7,3,8,4,2,0,1];
+for(var i = 0; i < arr.length; i++){ 
+    if(arr[i] % 2 == 0){
+        console.log(arr[i]);
+    } 
+    else{
+        console.log("That is odd!");
+    }
+}
+
+// OUTPUT That is odd!, That is odd!, 8, 4, 2, 0, That is odd!
---
+++ javascriptstck/arrays/predict3.js
@@ -0,0 +1,18 @@
+var arr = [1,3,8,-5,0,-2,4,-1];
+var newArr = [];
+for(var i = 0; i< arr.length; i++){
+    if(arr[i] < 0){
+        newArr.push(arr[i]);
+        arr[i] = arr[i] * -1;
+    }
+    else if(arr[i] == 0){
+        arr[i] = "Zero";
+    }
+    else{
+        arr[i] = arr[i] * -1;
+    }
+}
+console.log(arr);
+console.log(newArr);
+
+//Array(8)[-1,-3,-8,5,"zero",2,-4,1] Array(3) [-5-2,-1]
